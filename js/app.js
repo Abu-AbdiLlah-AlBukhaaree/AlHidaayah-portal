8cca8aa6e4317bc69a557297659a5d9da87e0587
unknown <faisolbamigbade@gmail.com>
2023-08-11 04:28:14 +0100

    added modal functionality to index.html

--- js/app.js
+++ js/app.js
@@ -3,9 +3,3 @@ const modal = document.getElementById('modal');
 window.addEventListener('load', () => {
   modal.style.display = 'flex';
 });
-
-modal.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-});
